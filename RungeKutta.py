b55791862e0639f94cd1be13932738ccb9e6d98c
Marian Horn <marianhorn@gmx.de>
2025-04-01 19:56:22 +0200

    runge kutta error remaining

--- RungeKutta.py
+++ RungeKutta.py
@@ -1,9 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import gmpy2
+from gmpy2 import mpfr, sin, exp, const_pi
 
-# Exact solution
+# Set precision
+gmpy2.get_context().precision = 256
+pi = const_pi()
+
+# Exact solution using gmpy2
 def exact_solution(x, t):
-    return np.exp(np.sin(x - 2 * np.pi * t))
+    return np.array([float(exp(sin(mpfr(xi) - 2 * pi * mpfr(t)))) for xi in x], dtype=np.float64)
 
 # Initial condition
 def initial_condition(x):
@@ -17,14 +23,24 @@ def derivative_fd2(u, dx):
 def derivative_fd4(u, dx):
     return (-np.roll(u, 2) + 8 * np.roll(u, 1) - 8 * np.roll(u, -1) + np.roll(u, -2)) / (12 * dx)
 
-# Fourier differentiation matrix
-def fourier_diff_matrix(N):
-    D = np.zeros((N, N))
+# Fourier differentiation matrix using gmpy2 for high precision
+def fourier_diff_matrix_gmp(N):
+    D = [[mpfr(0) for _ in range(N)] for _ in range(N)]
     for j in range(N):
         for i in range(N):
             if i != j:
-                D[j, i] = (-1)**(j + i) / (2 * np.sin((j - i) * np.pi / N))
+                angle = (j - i) * pi / mpfr(N)
+                D[j][i] = ((-1)**(j + i)) / (2 * sin(angle))
     return D
+
+# Matrix-vector multiplication using gmpy2 matrix
+def matvec_gmp(D, u):
+    N = len(u)
+    result = [mpfr(0) for _ in range(N)]
+    for j in range(N):
+        for i in range(N):
+            result[j] += D[j][i] * mpfr(u[i])
+    return np.array([float(-2 * pi * r) for r in result], dtype=np.float64)
 
 # Time stepping using 4th order Runge-Kutta
 def rk4_step(u, dt, derivative_func):
@@ -36,7 +52,7 @@ def rk4_step(u, dt, derivative_func):
 
 # Solver
 def solve_pde(N, T, dt, method='fd2'):
-    x = np.linspace(0, 2*np.pi, N, endpoint=False)
+    x = np.linspace(0, 2 * np.pi, N, endpoint=False)
     dx = x[1] - x[0]
     u = initial_condition(x)
 
@@ -45,8 +61,8 @@ def solve_pde(N, T, dt, method='fd2'):
     elif method == 'fd4':
         derivative = lambda u: -2 * np.pi * derivative_fd4(u, dx)
     elif method == 'fourier':
-        D = fourier_diff_matrix(N)
-        derivative = lambda u: -2 * np.pi * D @ u
+        D = fourier_diff_matrix_gmp(N)
+        derivative = lambda u: matvec_gmp(D, u)
     else:
         raise ValueError("Unknown method")
 
